891be4dc1bb934b11eca26f174abe568fe346b25
serdar.baykan@siemens.com <serdar.baykan@siemens.com>
2017-11-20 21:05:00 +0300

    initial comit

---
+++ src/main/java/org/serdar/deeplearning/Controller.java
@@ -0,0 +1,14 @@
+package org.serdar.deeplearning;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
